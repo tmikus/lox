f5efcadd7c7c0ec076f219afbfdc62f510d8e086
Tomasz Mikus <mikus.tomasz@gmail.com>
2017-11-12 17:46:46 +0000

    Correctly generate the AST

--- src/lox/expressions/Expr.kt
+++ src/lox/expressions/Expr.kt
@@ -2,13 +2,38 @@ package lox.expressions
 
 import lox.Token
 
-interface Expr
+abstract class Expr() {
+  abstract fun <R> accept(visitor: Visitor<R>): R
+}
 
-data class Grouping(val expression: Expr): Expr
+interface Visitor<R> {
+  fun visitGroupingExpr(grouping: Grouping): R
+  fun visitBinaryExpr(binary: Binary): R
+  fun visitUnaryExpr(unary: Unary): R
+  fun visitLiteralExpr(literal: Literal): R
+}
 
-data class Binary(val left: Expr, val operator: Token, val right: Expr): Expr
+data class Grouping(val expression: Expr): Expr() {
+  override fun <R> accept(visitor: Visitor<R>): R {
+    return visitor.visitGroupingExpr(this)
+  }
+}
 
-data class Unary(val operator: Token, val right: Expr): Expr
+data class Binary(val left: Expr, val operator: Token, val right: Expr): Expr() {
+  override fun <R> accept(visitor: Visitor<R>): R {
+    return visitor.visitBinaryExpr(this)
+  }
+}
 
-data class Literal(val value: Any): Expr
+data class Unary(val operator: Token, val right: Expr): Expr() {
+  override fun <R> accept(visitor: Visitor<R>): R {
+    return visitor.visitUnaryExpr(this)
+  }
+}
+
+data class Literal(val value: Any): Expr() {
+  override fun <R> accept(visitor: Visitor<R>): R {
+    return visitor.visitLiteralExpr(this)
+  }
+}
 
--- src/lox/tools/GenerateAst.kt
+++ src/lox/tools/GenerateAst.kt
@@ -24,7 +24,11 @@ fun defineAst(outputDir: String, baseName: String, types: HashMap<String, String
 	writer.println()
 	writer.println("import lox.Token")
 	writer.println()
-	writer.println("interface $baseName")
+	writer.println("abstract class $baseName() {")
+  writer.println("  abstract fun <R> accept(visitor: Visitor<R>): R")
+  writer.println("}")
+
+  defineVisitor(writer, baseName, types)
 
 	types.forEach { (className, fields) -> defineType(writer, baseName, className, fields) }
 
@@ -32,7 +36,20 @@ fun defineAst(outputDir: String, baseName: String, types: HashMap<String, String
 	writer.close()
 }
 
+fun defineVisitor(writer: PrintWriter, baseName: String, types: HashMap<String, String>) {
+  writer.println()
+  writer.println("interface Visitor<R> {")
+  types.forEach { (className, _) ->
+    writer.println("  fun visit$className$baseName(${className.toLowerCase()}: $className): R")
+  }
+  writer.println("}")
+}
+
 fun defineType(writer: PrintWriter, baseName: String, className: String, fields: String) {
 	writer.println()
-	writer.println("data class $className($fields): $baseName")
+	writer.println("data class $className($fields): $baseName() {")
+  writer.println("  override fun <R> accept(visitor: Visitor<R>): R {")
+  writer.println("    return visitor.visit$className$baseName(this)")
+  writer.println("  }")
+  writer.println("}")
 }
